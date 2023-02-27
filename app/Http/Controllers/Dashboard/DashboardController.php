3ff8695c150284f9f94ec64802d89431d226ca78
Oduah Michael <oduahmichaelebuka@gmail.com>
2023-02-27 18:40:32 +0100

    Added the colour for the buttons

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Models\Payment;
+use App\Models\News;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
-    // 
+    //
     public function index() {
         $payments = Payment::all();
+        $news = News::all();
         return view('dashboard.index', ['payments' => $payments]);
     }
 
-    
+
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,10 +5,10 @@
         <table id="myTable" class="table table-striped table-bordered">
             <thead>
                 <tr>
+                    <th scope="col">Time Payed</th>
                     <th scope="col">Name</th>
                     <th scope="col">Amount</th>
                     <th scope="col">Payment Type</th>
-                    <th scope="col">Time Payed</th>
                     <th scope="col">Status</th>
                     <th scope="col">Validate</th>
                 </tr>
@@ -20,16 +20,24 @@
                     $id= $payment->id
                 @endphp
                     <tr style="background: none">
+                        <td> {{ $payment->created_at }} </td>
                         <td> {{ $payment->name }} </td>
                         <td> {{ $payment->amount }} </td>
                         <td> {{ $payment->payment }} </td>
-                        <td> {{ $payment->created_at }} </td>
                         <td>
-                            <button class="btn btn-sm btn-primary"> {{ $payment->status }} </button>
+                            @if ($payment->status == "Approved")
+                                <button class="btn btn-sm btn-success"> {{ $payment->status }} </button>
+
+                            @elseif ($payment->status == "Decline")
+                                <button class="btn btn-sm btn-danger"> {{ $payment->status }} </button>
+                            @else
+                                <button class="btn btn-sm btn-primary"> {{ $payment->status }} </button>
+                            @endif
+
                         </td>
                         <td>
-                            <a href="{{ route('dashboard.approve', $payment->id) }}" class="btn btn-sm btn-outline-success" onclick="geek()">Approve</a>
-                            <a href="{{ route('dashboard.decline', $payment->id) }}" class="btn btn-sm btn-outline-danger" onclick="geek()">Decline</a>
+                            <a href="{{ route('dashboard.approve', $payment->id) }}" class="btn btn-sm btn-outline-success m-1" onclick="geek()">Approve</a>
+                            <a href="{{ route('dashboard.decline', $payment->id) }}" class="btn btn-sm btn-outline-danger m-1" onclick="geek()">Decline</a>
 
                             <script>
                                 function geek() {
@@ -66,8 +74,8 @@
     {{-- ------------------ Payment Form ------------------- --}}
 
     {{-- ------------------ News and Event Form ------------------- --}}
-    <h1><strong class="text-center">News and Event Form</strong></h1>
-        <div class="container mt-5">
+    <h1 class="mt-5"><strong class="text-center">News and Event Form</strong></h1>
+        <div class="container">
             <form method="POST" action="{{ route('dashboard.new') }}" enctype="multipart/form-data">
                 @csrf
                 <label for="title" class="form-label">Title: </label>
@@ -87,11 +95,18 @@
         </div>
     {{-- ------------------ News and Event Form ------------------- --}}
 
-    {{-- ------------------ News and Event Form ------------------- --}}
-    <h1><strong class="text-center">News and Event Form</strong></h1>
-        <div class="container mt-5">
+    {{-- ------------------ News and Event Form View ------------------- --}}
+        <div class="row mt-5">
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
         </div>
-    {{-- ------------------ News and Event Form ------------------- --}}
+    {{-- ------------------ News and Event Form View ------------------- --}}
 
 
 @endsection
