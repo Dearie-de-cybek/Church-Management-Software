d2e9090a3d22cbe1f5baa1f62b5604ae1dc0d285
Michael Oduah <oduahmichaelebuka@gmail.com>
2023-06-23 12:10:35 +0100

    UPDATED

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,65 +1,35 @@
 'use strict';
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('nav').classList.toggle("open")
+})
 
-///////////////////////////////////////////////////////////
-// Make mobile navigation work
+document.querySelector('.close-btn').addEventListener('click', () =>{
+    document.querySelector('nav').classList.toggle("open")
+})
 
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
 
-// Smooth scrolling animation
-
-const allLinks = document.querySelectorAll("a:link");
-
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    
-    const href = link.getAttribute("href");
-
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth", 
-      });
-
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
+var mySwiper = new Swiper ('.swiper-container', {
+    speed: 400,
+    spaceBetween: 50,
+    initialSlide: 0,
+    //truewrapper adoptsheight of active slide
+    autoHeight: true,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    loopAdditionalSlides: 2,
+    loopedSlides: 5,
+    // delay between transitions in ms
+    autoplayStopOnLast: false
     
   });
-});
 
-///////////////////////////////////////////////////////////
-// Sticky navigation
 
-const sectionHeroEl = document.querySelector(".section-hero");
 
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    console.log(ent);
+  mySwiper.prependSlide(mySwiper.slides[mySwiper.slides.length - 1].outerHTML);
+  mySwiper.appendSlide(mySwiper.slides[1].outerHTML);
 
-    if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
-    }
-
-    if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // In the viewport
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
-obs.observe(sectionHeroEl);
-
-///////////////////////////////////////////////////////////
+  setInterval(function() {
+      mySwiper.slideNext();
+    }, 2000);
